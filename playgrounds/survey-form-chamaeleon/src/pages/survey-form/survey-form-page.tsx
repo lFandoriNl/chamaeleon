bbeeccada1248a8506e47a2116a87863a041181c
FandoriN <lkoylol12@gmail.com>
2023-12-02 17:45:26 +0200

    chore: add editor mode to playgrounds survey-form-chamaeleon

--- playgrounds/survey-form-chamaeleon/src/pages/survey-form/survey-form-page.tsx
+++ playgrounds/survey-form-chamaeleon/src/pages/survey-form/survey-form-page.tsx
@@ -1,17 +1,22 @@
-import { useEditor } from '@chamaeleon/react-editor';
+import { useEditorSelector } from '@chamaeleon/react-editor';
 import RedoIcon from '@mui/icons-material/Redo';
 import UndoIcon from '@mui/icons-material/Undo';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
+import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 
 import { SurveyForm } from './survey-form';
 
 export function SurveyFormPage() {
-  const editor = useEditor();
+  const [isViewMode, editor] = useEditorSelector(
+    ({ editor }) => editor.mode === 'view',
+  );
 
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
@@ -20,15 +25,29 @@ export function SurveyFormPage() {
           <Stack direction="row" justifyContent="space-between">
             <Typography variant="h5">Survey form</Typography>
 
-            <ButtonGroup variant="outlined">
-              <Button onClick={editor.commands.undo}>
-                <UndoIcon />
-              </Button>
+            <Box display="flex">
+              <FormControlLabel
+                control={
+                  <Switch
+                    value={isViewMode}
+                    onChange={(_, checked) => {
+                      editor.changeMode(checked ? 'view' : 'editor');
+                    }}
+                  />
+                }
+                label={isViewMode ? 'View mode' : 'Editor mode'}
+              />
 
-              <Button onClick={editor.commands.redo}>
-                <RedoIcon />
-              </Button>
-            </ButtonGroup>
+              <ButtonGroup variant="outlined">
+                <Button onClick={editor.commands.undo}>
+                  <UndoIcon />
+                </Button>
+
+                <Button onClick={editor.commands.redo}>
+                  <RedoIcon />
+                </Button>
+              </ButtonGroup>
+            </Box>
           </Stack>
         </Paper>
 
